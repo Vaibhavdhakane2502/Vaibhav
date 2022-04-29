38cdd4cbd61655a2dba102f5f7a0d83551df8904
hp <hp@Enma>
2022-04-29 09:44:48 +0530

    First Commit

---
+++ Oops/src/Package2/SuperClass.java
@@ -0,0 +1,18 @@
+package Package2;
+
+public class SuperClass  //static method 
+{
+	public static void test1()
+	{
+		System.out.println(" My name is Vaibhav");
+	}
+	public static void test2()
+	{
+		System.out.println("My name is Sahil");
+	}
+	public static void test3()
+	{
+		System.out.println(" My name is Ritesh");
+	}
+	
+}
