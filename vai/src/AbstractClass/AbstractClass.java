2a52ed0d8b1d285a2f15040e42980dd63458c804
hp <hp@Enma>
2022-04-29 09:45:31 +0530

    First Commit

---
+++ vai/src/AbstractClass/AbstractClass.java
@@ -0,0 +1,10 @@
+package AbstractClass;
+
+public abstract class AbstractClass { //Abstract class
+	public void monday() {
+		System.out.println("This is monday");  //complete method	
+	}
+	 public abstract void tuesday(); //incomplete
+	 public abstract void thursday();//
+		
+	}
